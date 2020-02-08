3457242db9039287af3d93adb79ba2bde55035b7
Rez <reza.amini.majd1379@gmail.com>
2020-02-08 10:30:40 +0330

    AdEvent Model Compelte

--- src/main/kotlin/Ad/model/AdEvent.kt
+++ src/main/kotlin/Ad/model/AdEvent.kt
@@ -1,2 +1,9 @@
 package Ad.model
 
+data class AdEvent(val requestId: String,
+                   val adId: String,
+                   val adTitle: String,
+                   val advertiserCost: Double,
+                   val appId: String,
+                   val appTitle: String,
+                   val impressionTime: Long)
